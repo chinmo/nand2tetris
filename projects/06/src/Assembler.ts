631b3124222bddc5007e6e20fe123089ecc6c3ba
chinmo <chinmo@gmail.com>
2021-10-08 22:45:12 +0900

    Refactoring: remove unnecessary codes.

--- projects/06/src/Assembler.ts
+++ projects/06/src/Assembler.ts
@@ -1,10 +1,8 @@
 import fs from "fs";
 import path from "path";
-import { L_COMMAND, Parser } from "./parser";
-import { A_COMMAND, C_COMMAND } from "../src/parser";
-import { dest, comp, jump } from "../src/code";
+import { A_COMMAND, C_COMMAND, L_COMMAND, Parser } from "./parser";
+import { dest, comp, jump } from "./code";
 import { SymbolTable } from "./symbol_table";
-import { isNamedExportBindings } from "typescript";
 
 const argPath: string = process.argv[2];
 if (argPath) assembleFromFile(argPath);
@@ -55,23 +53,17 @@ function exec2ndPass(asmPath: string, symbolTable: SymbolTable) {
         let address = 0;
         const Xxx = parser.symbol();
         if (symbolTable.contains(Xxx)) {
-          // Xxx = symbolTable.getAddress(Xxx).toString(10);
           address = symbolTable.getAddress(Xxx);
         } else {
           address = parseInt(Xxx);
           if (isNaN(address)) {
-            // Variable label
             console.log(Xxx);
             address = allocatableAddress;
             symbolTable.addEntry(Xxx, address);
             allocatableAddress++;
           }
         }
-        fs.writeSync(
-          fd,
-          // parseInt(Xxx, 10).toString(2).padStart(16, "0") + "\n"
-          address.toString(2).padStart(16, "0") + "\n"
-        );
+        fs.writeSync(fd, address.toString(2).padStart(16, "0") + "\n");
         break;
       }
       case C_COMMAND:
@@ -85,7 +77,6 @@ function exec2ndPass(asmPath: string, symbolTable: SymbolTable) {
         );
         break;
       case L_COMMAND:
-        console.log("L_COMMAND!");
         break;
       default:
         throw new Error("2nd Pass: Unknown command type");
