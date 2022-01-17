56a476e2fcd44555c5f6db265b40589ed8841e27
chinmo <chinmo@gmail.com>
2022-01-17 20:32:28 +0900

    Add constructor

--- projects/07/src/parser.ts
+++ projects/07/src/parser.ts
@@ -1,1 +1,45 @@
-export class Parser {}
+import fs from "fs";
+
+const Command = {
+  Arithmetic: 0,
+  Push: 1,
+  Pop: 2,
+  Label: 3,
+  Goto: 4,
+  If: 5,
+  Function: 6,
+  Return: 7,
+  Call: 8,
+} as const;
+
+export class Parser {
+  lines: string[];
+
+  constructor(vmFilePath: string) {
+    this.lines = [];
+
+    try {
+      const buffer = fs.readFileSync(vmFilePath);
+      if (buffer) {
+        buffer
+          .toString()
+          .split("\n")
+          .map((line) => {
+            if (line) {
+              this.lines.push(line);
+            }
+          });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  hasMoreCommands(): boolean {
+    return this.lines.length > 0;
+  }
+
+  advance(): any {
+    throw new Error("Illegal operation.");
+  }
+}
--- projects/07/test/parser.test.ts
+++ projects/07/test/parser.test.ts
@@ -1,13 +1,60 @@
 import { Parser } from "../src/parser";
 
-describe("constructor", () => {
-  test("Initial state", () => {
+import fs from "fs";
+import path from "path";
+
+const vmPath = path.join(__dirname, "Hoge.vm");
+
+describe("Initial State", () => {
+  afterEach(() => {
+    deleteVmFile();
+  });
+
+  test("When file is not exist", () => {
+    // Given
+    // When
+    const parser = new Parser("FilePathIsNotExist");
+
+    // Then
+    expect(parser.hasMoreCommands()).toBeFalsy();
+    expect(typeof parser.hasMoreCommands()).toBe("boolean");
+    expect(() => {
+      parser.advance();
+    }).toThrowError();
+  });
+
+  test("Empty file", () => {
+    // Given
+    fs.writeFileSync(vmPath, "");
+
+    // When
+    const parser = new Parser(vmPath);
+
+    // Then
+    expect(parser.hasMoreCommands()).toBeFalsy();
+  });
+
+  test("Valid vm file", () => {
     // Given
 
     // When
-    const parser = new Parser();
+    const parser = new Parser("StackArithmetic/SimpleAdd/SimpleAdd.vm");
 
     // Then
-    expect(parser).not.toBeNull();
+    expect(parser.hasMoreCommands()).toBeTruthy();
   });
 });
+
+function deleteVmFile() {
+  fs.readdirSync(__dirname)
+    .filter((f) => f.endsWith(".vm"))
+    .map((f) => unlink(path.join(__dirname, f)));
+}
+
+function unlink(path: string): void {
+  try {
+    fs.unlinkSync(path);
+  } catch (err) {
+    console.log(err);
+  }
+}
