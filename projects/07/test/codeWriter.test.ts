7a5dcd6fe2b5ab5cb3db7e5ffddc3c17da495abd
chinmo <chinmo@gmail.com>
2022-05-03 14:15:11 +0900

    First test of CodeWriter

--- projects/07/test/codeWriter.test.ts
+++ projects/07/test/codeWriter.test.ts
@@ -1,1 +1,10 @@
 import { CodeWriter } from "../src/codeWriter"
+
+describe("Initial state", () => {
+    test("When there is no filename", () => {
+        // Given
+        // When
+        const writer = new CodeWriter();
+        // Then
+    })
+})
