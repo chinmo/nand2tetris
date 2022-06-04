9b604c40b3b72320286c39fa111ad46d8b2cf79a
chinmo <chinmo@gmail.com>
2022-06-04 17:34:05 +0900

    Pass single file test

--- projects/07/src/vmTranslator.ts
+++ projects/07/src/vmTranslator.ts
@@ -1,4 +1,36 @@
-export function vmTranslator(path: string): void {
-  console.log(path);
-  return;
+import fs from "fs";
+import path from "path";
+import { CodeWriter } from "./codeWriter";
+
+vmTranslator(process.argv[2]);
+
+export async function vmTranslator(vmPath: string): Promise<unknown> {
+  if (!fs.existsSync(vmPath)) return;
+
+  const asmPath = path.join(
+    path.dirname(vmPath),
+    path.basename(vmPath, ".vm") + ".asm"
+  );
+
+  const stream = fs
+    .createWriteStream(asmPath, { encoding: "utf-8" })
+    .on("error", (err) => {
+      console.log(err);
+    });
+
+  const writer = new CodeWriter(stream);
+  writer.setFileName(path.basename(vmPath));
+  writer.close();
+
+  return await waitWriteStreamFinished(stream);
+}
+
+async function waitWriteStreamFinished(
+  stream: fs.WriteStream
+): Promise<unknown> {
+  return new Promise((resolve) => {
+    stream.on("finish", () => {
+      resolve("finish writeStream");
+    });
+  });
 }
--- projects/07/test/vmTranslator.test.ts
+++ projects/07/test/vmTranslator.test.ts
@@ -1,9 +1,26 @@
+import fs from "fs";
 import { vmTranslator } from "../src/vmTranslator";
+import { deleteTestFiles } from "./fileUtil";
+
 describe("File Handling", () => {
-  test("No Arguments", () => {
+  afterEach(() => {
+    deleteTestFiles();
+  });
+
+  test("Invalid path", async () => {
     // Given
     // When
+    await vmTranslator("InvalidPath.vm");
     // Then
-    expect(vmTranslator("")).toBeUndefined();
+    expect(fs.existsSync("InvalidPath.asm")).toBeFalsy();
+  });
+
+  test("Single file", async () => {
+    // Given
+    fs.writeFileSync("Hoge.vm", "");
+    // When
+    await vmTranslator("Hoge.vm");
+    // Then
+    expect(fs.existsSync("Hoge.asm")).toBeTruthy();
   });
 });
